35e2dcbccf685063030d1f6a3d5d27be25a58bc9
Allen Guo <ag1465@scarletmail.rutgers.edu>
2019-09-23 14:53:47 -0400

    test not working

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -5,28 +5,76 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
+const Product = db.model('product')
+const Order = db.model('order')
 
 describe('User routes', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
 
-  describe('/api/users/', () => {
+  describe('/api/users/cart', () => {
     const codysEmail = 'cody@puppybook.com'
 
-    beforeEach(() => {
-      return User.create({
-        email: codysEmail
-      })
-    })
+    // beforeEach(() => {
+    //   const user =  User.create({
+    //     email: codysEmail,
+    //     password: '123'
+    //   })
+    // })
 
-    it('GET /api/users', async () => {
+    it('GET /api/users/cart', async () => {
+      const user = User.create({
+        email: codysEmail,
+        password: '123'
+      })
       const res = await request(app)
-        .get('/api/users')
+        .get(`/api/users/1/cart`)
         .expect(200)
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
+      expect(res.body).to.be.an('object')
+      expect(res.body.userId).to.be.equal(user.id)
     })
+
+    it('POST /api/users/1/cart/add/1', async () => {
+      const user = await User.create({
+        email: codysEmail,
+        password: '123'
+      })
+      const order = await Order.create({
+        userId: user.id,
+        status: 'open'
+      })
+      console.log(user)
+      const product = await Product.create({
+        name: 'Health Potion',
+        imageUrl: '/images/products/potionRed.png',
+        description: 'test Potion',
+        stock: '30',
+        price: '750'
+      })
+      console.log(product)
+      const res = await request(app)
+        .post(`/api/users/1/cart/add/1`)
+        .expect(201)
+
+      const res2 = await request(app)
+        .get(`/api/users/1/cart`)
+        .expect(200)
+
+      expect(res2.body).to.be.an('object')
+      expect(res2.body.orderProducts[0].productId).to.be.equal(product.id)
+    })
+    // it('GET /api/users/1/cart', async () => {
+    //   const res = await request(app)
+    //     .get('/api/users/1/cart')
+    //     .expect(200)
+
+    //   expect(res.body).to.be.an('object')
+    //   expect(res.body[0].userId).to.be.equal(1)
+
+    // })
+
+    // it('GET /api/users/1')
   }) // end describe('/api/users')
 }) // end describe('User routes')
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -6,10 +6,18 @@ const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
   },
   password: {
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    },
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
